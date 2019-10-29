f437a704c87eafbe6b341536b986e1d00cbc5f20
ukieTux <ukie.tux@gmail.com>
2019-10-29 22:58:25 +0800

    Add color resources

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -3,6 +3,22 @@ import 'package:flutter/painting.dart';
 
 class Pallette {
   static const MaterialColor colorPrimary = const MaterialColor(
+    0xff7b68ee,
+    const <int, Color>{
+      50: const Color(0xff7b68ee),
+      100: const Color(0xff7b68ee),
+      200: const Color(0xff7b68ee),
+      300: const Color(0xff7b68ee),
+      400: const Color(0xff7b68ee),
+      500: const Color(0xff7b68ee),
+      600: const Color(0xff7b68ee),
+      700: const Color(0xff7b68ee),
+      800: const Color(0xff7b68ee),
+      900: const Color(0xff7b68ee),
+    },
+  );
+
+  static const MaterialColor colorPrimaryDark = const MaterialColor(
     0xff6A5ACD,
     const <int, Color>{
       50: const Color(0xff6A5ACD),
@@ -18,5 +34,6 @@ class Pallette {
     },
   );
 
-  static const textPrimary = Color(0xff000000);
+  static const textPrimary = Color(0xff363636);
+  static const textDisable = Color(0xff909090);
 }
